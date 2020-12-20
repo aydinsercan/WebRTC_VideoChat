7dc82fcf641b8e90c3b8f67b2f0fb960dafd3403
aydinsercan <aydinser17@itu.edu.tr>
2020-12-20 17:24:32 +0300

    update

---
+++ src/MessageBox.js
@@ -0,0 +1,73 @@
+import React from "react";
+import {
+  Header,
+  Icon,
+  Input,
+  Grid,
+  Segment,
+  Card,
+  Sticky,
+  Button,
+  Comment
+} from "semantic-ui-react";
+import { formatRelative } from "date-fns";
+import avatar from "./avatar.png";
+
+const MessageBox = ({ messages, connectedTo, message, setMessage, sendMsg, name }) => {
+  return (
+    <Grid.Column width={11}>
+      <Sticky>
+        <Card fluid>
+          <Card.Content
+            header={
+              !!connectedTo ? connectedTo : ""
+            }
+          />
+          <Card.Content>
+            {!!connectedTo && messages[connectedTo] ? (
+              <Comment.Group>
+                {messages[connectedTo].map(({ name: sender, message: text, time }) => (
+                  <Comment key={`msg-${name}-${time}`}>
+                    <Comment.Avatar src={avatar} />
+                    <Comment.Content>
+                      <Comment.Author>{sender === name ? 'You' : sender}</Comment.Author>
+                      <Comment.Metadata>
+                        <span>
+                          {formatRelative(new Date(time), new Date())}
+                        </span>
+                      </Comment.Metadata>
+                      <Comment.Text>{text}</Comment.Text>
+                    </Comment.Content>
+                  </Comment>
+                ))}
+              </Comment.Group>
+            ) : (
+              <Segment placeholder>
+                <Header icon>
+                  <Icon name="discussions" />
+                  No Messages
+                </Header>
+              </Segment>
+            )}
+            <Input
+              fluid
+              type="text"
+              value={message}
+              onChange={e => setMessage(e.target.value)}
+              placeholder="Type message"
+              action
+            >
+              <input />
+              <Button color="teal" disabled={!message} onClick={sendMsg}>
+                <Icon name="send" />
+                Send
+              </Button>
+            </Input>
+          </Card.Content>
+        </Card>
+      </Sticky>
+    </Grid.Column>
+  );
+};
+
+export default MessageBox;
